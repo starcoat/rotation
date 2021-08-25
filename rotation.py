f79189f0d38ed29ec766aac57b11fd4b742249dc
Christian Zieske <me@ekseiz.com>
2021-08-25 23:17:54 +0200

    Signaturen für Stick und Playground

---
+++ rotation.py
@@ -0,0 +1,13 @@
+Coordinates = tuple[int, int]
+
+
+class Stick:
+
+    def __init__(self, symbol: str, coords: list[Coordinates]) -> None:
+        pass
+
+
+class Playground:
+
+    def __init__(self, size: int, door: Coordinates) -> None:
+        pass
