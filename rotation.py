27ce586eff87ee23ee13dd4da5a4e2baaddc40ec
Christian Zieske <me@ekseiz.com>
2021-08-25 23:21:14 +0200

    Drehung, Sticks-Array und Leinwand initialisieren

--- rotation.py
+++ rotation.py
@@ -13,6 +13,14 @@ class Playground:
     def __init__(self, size: int, door: Coordinates) -> None:
         self.size = size
         self.door = door
+        self.rotation = 0
+        self.sticks = []
+
+        # Leinwand initialisieren
+        self.canvas = [[' ' for x in range(size)] for y in range(size)]
+        for s in range(size):
+            self.canvas[s][0] = self.canvas[0][s] = self.canvas[s][size - 1] = self.canvas[size - 1][s] = '#'
+        self.canvas[door[0]][door[1]] = ' '
 
     def __str__(self) -> str:
         pass
