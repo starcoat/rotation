e9905c970f2b02400273a5b19e08f27c535692b9
Christian Zieske <me@ekseiz.com>
2021-08-26 23:27:22 +0200

    Tests für rotate(); Tests umbenannt, umstrukturiert

--- test_pytest.py
+++ test_pytest.py
@@ -1,8 +1,29 @@
 import rotation
 
+
+pg_test1 = rotation.Playground(8, (7,5))
+
+pg_test2 = rotation.Playground(7, (6,3))
+pg_test2.add_stick(rotation.Stick('0', [(2,2),(3,2),(4,2),(5,2)]))
+
+pg_test3 = rotation.Playground(10, (3,9))
+pg_test3.add_stick(rotation.Stick('0', [(1,1),(1,2)]))
+pg_test3.add_stick(rotation.Stick('1', [(1,3),(2,3),(3,3)]))
+pg_test3.add_stick(rotation.Stick('2', [(8,8),(7,8),(6,8),(5,8)]))
+pg_test3.add_stick(rotation.Stick('3', [(4,8),(4,7),(4,6),(4,5),(4,4),(4,3)]))
+
+pg_test4 = rotation.Playground(8, (7,3))
+pg_test4.add_stick(rotation.Stick('0', [(1,6),(2,6)]))
+pg_test4.add_stick(rotation.Stick('1', [(3,1),(3,2)]))
+pg_test4.add_stick(rotation.Stick('2', [(3,3),(3,4),(3,5),(3,6)]))
+pg_test4.add_stick(rotation.Stick('3', [(4,1),(4,2)]))
+pg_test4.add_stick(rotation.Stick('4', [(4,6),(5,6),(6,6)]))
+pg_test4.add_stick(rotation.Stick('5', [(5,1),(5,2)]))
+pg_test4.add_stick(rotation.Stick('6', [(6,1),(6,2),(6,3)]))
+
+
 def test_playground1():
-    pg_test = rotation.Playground(8, (7,5))
-    assert pg_test.__str__() == \
+    assert pg_test1.__str__() == \
         """\
 ########
 #      #
@@ -15,9 +36,7 @@ def test_playground1():
 """
 
 def test_playground2():
-    pg_test = rotation.Playground(7, (6,3))
-    pg_test.add_stick(rotation.Stick('0', [(2,2),(3,2),(4,2),(5,2)]))
-    assert pg_test.__str__() == """\
+    assert pg_test2.__str__() == """\
 #######
 #     #
 # 0   #
@@ -28,12 +47,7 @@ def test_playground2():
 """
 
 def test_playground3():
-    pg_test = rotation.Playground(10, (3,9))
-    pg_test.add_stick(rotation.Stick('0', [(1,1),(1,2)]))
-    pg_test.add_stick(rotation.Stick('1', [(1,3),(2,3),(3,3)]))
-    pg_test.add_stick(rotation.Stick('2', [(8,8),(7,8),(6,8),(5,8)]))
-    pg_test.add_stick(rotation.Stick('3', [(4,8),(4,7),(4,6),(4,5),(4,4),(4,3)]))
-    assert pg_test.__str__() == """\
+    assert pg_test3.__str__() == """\
 ##########
 #001     #
 #  1     #
@@ -46,16 +60,8 @@ def test_playground3():
 ##########
 """
 
-def test_rotation1():
-    pg_test = rotation.Playground(8, (7,3))
-    pg_test.add_stick(rotation.Stick('0', [(1,6),(2,6)]))
-    pg_test.add_stick(rotation.Stick('1', [(3,1),(3,2)]))
-    pg_test.add_stick(rotation.Stick('2', [(3,3),(3,4),(3,5),(3,6)]))
-    pg_test.add_stick(rotation.Stick('3', [(4,1),(4,2)]))
-    pg_test.add_stick(rotation.Stick('4', [(4,6),(5,6),(6,6)]))
-    pg_test.add_stick(rotation.Stick('5', [(5,1),(5,2)]))
-    pg_test.add_stick(rotation.Stick('6', [(6,1),(6,2),(6,3)]))
-    assert pg_test.__str__() == """\
+def test_playground4():
+    assert pg_test4.__str__() == """\
 ########
 #     0#
 #     0#
@@ -64,4 +70,30 @@ def test_rotation1():
 #55   4#
 #666  4#
 ### ####
+"""
+
+def test_rotate1():
+    pg_test1.rotate('CW')
+    assert pg_test1.__str__() == """\
+########
+#      #
+#      #
+#      #
+#      #
+       #
+#      #
+########
+"""
+
+def test_rotate4():
+    pg_test4.rotate('CW')
+    assert pg_test4.__str__() == """\
+########
+#6531  #
+#6531  #
+ 6  2  #
+#   2  #
+#   2  #
+#444200#
+########
 """
