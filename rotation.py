2475a3a6e547632f20f2e5d11b06954779eba0bc
Christian Zieske <me@ekseiz.com>
2021-08-28 11:31:27 +0200

    Fallunterscheidung für ROtation in Hilfsfunktionen ausgelagert

--- rotation.py
+++ rotation.py
@@ -22,29 +22,43 @@ class Playground:
 			self.canvas[s][0] = self.canvas[0][s] = self.canvas[s][size - 1] = self.canvas[size - 1][s] = '#'
 		self.canvas[door[0]][door[1]] = ' '
 
+	def rotate_help_x(self, x, y):
+		result = {0:x, 90:self.size - 1 - y, 180:self.size - 1 - x, 270:y}
+		return result[self.rotation]
+
+	def rotate_help_y(self, x, y):
+		result = {0:y, 90:x, 180:self.size - 1 - y, 270:self.size - 1 - x}
+		return result[self.rotation]
+
 	def __str__(self) -> str:
 		result = ''
 
-		if self.rotation == 0:
-			for x in range(self.size):
-				for y in range(self.size):
-					result += self.canvas[x][y]
-				result += '\n'
-		elif self.rotation == 90:
-			for x in range(self.size):
-				for y in range(self.size):
-					result += self.canvas[self.size - 1 - y][x]
-				result += '\n'
-		elif self.rotation == 180:
-			for x in range(self.size):
-				for y in range(self.size):
-					result += self.canvas[self.size - 1 - x][self.size - 1 - y]
-				result += '\n'
-		elif self.rotation == 270:
-			for x in range(self.size):
-				for y in range(self.size):
-					result += self.canvas[y][self.size - 1 - x]
-				result += '\n'
+		# if self.rotation == 0:
+		# 	for x in range(self.size):
+		# 		for y in range(self.size):
+		# 			result += self.canvas[x][y]
+		# 		result += '\n'
+		# elif self.rotation == 90:
+		# 	for x in range(self.size):
+		# 		for y in range(self.size):
+		# 			result += self.canvas[self.size - 1 - y][x]
+		# 		result += '\n'
+		# elif self.rotation == 180:
+		# 	for x in range(self.size):
+		# 		for y in range(self.size):
+		# 			result += self.canvas[self.size - 1 - x][self.size - 1 - y]
+		# 		result += '\n'
+		# elif self.rotation == 270:
+		# 	for x in range(self.size):
+		# 		for y in range(self.size):
+		# 			result += self.canvas[y][self.size - 1 - x]
+		# 		result += '\n'
+
+		for x in range(self.size):
+			for y in range(self.size):
+				result += self.canvas[self.rotate_help_x(x, y)][self.rotate_help_y(x, y)]
+			result += '\n'
+
 		
 		return result
 
